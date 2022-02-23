4ce73d8f03e195cfd5d789eafe3fbb52607328e1
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-23 13:27:45 -0800

    Use express.json middleware

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const { getNewWord } = require('./Routes/word.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Get response from server! at / endpoint.')
 })
 
 //Word
+  //Where should this word be stored? How should this word be checked?
 app.get('/new-word', getNewWord);
 
 app.listen(port, () => {
