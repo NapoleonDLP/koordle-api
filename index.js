2e33b5df7e1f15ed32fda95b55f25adbabe5c868
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-23 15:02:40 -0800

    Enable cors

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const { getNewWord } = require('./Routes/word.js');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Get response from server! at / endpoint.')
