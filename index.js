495f836d2af93dc6fa8bb29e351f23cde21f386d
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-23 12:49:37 -0800

    Import getNewWord and use dotenv

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+require('dotenv').config();
+const { getNewWord } = require('./Routes/word.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Get response from server! at / endpoint.')
 })
+
+//Word
+app.get('/new-word', getNewWord);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
