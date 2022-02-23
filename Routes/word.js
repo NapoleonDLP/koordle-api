4c9ebcd57e743549d6dca6da5638c11c92f05fc1
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-23 13:27:03 -0800

    Res with random word

--- Routes/word.js
+++ Routes/word.js
@@ -1,7 +1,11 @@
 const DB = require('../db.js');
+const randomWord = require('random-word-by-length');
 
-const getNewWord = () => {
+const getNewWord = (req, res) => {
+  const newWord = randomWord(5);
 
+  res.send({ newWord });
 };
 
-module.exports = { getNewWord };
+module.exports = {
+  getNewWord };
