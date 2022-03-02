a482f7b74575bc0a5408f7ca9782ecccd865f8c8
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-02 14:44:25 -0800

    Write new Game to DB

--- Routes/game.js
+++ Routes/game.js
@@ -1,5 +1,6 @@
-const DB = require('../db.js');
+const { DB } = require('../db.js');
 const { generateWord } = require('../helpers.js');
+const { Game } = require('../Models/Game.js');
 
 //TODO: find a better solution for acquiring random word
 const getNewWord = (req, res) => {
@@ -17,9 +18,22 @@ const isWordValid = (req, res) => {
 
 //Create a new game
 const postNewGame = (req, res) => {
+  const wordLength = req.body.wordLength || 5;
   // declare data object
+  let data = {};
   // declare new word
+  data.word = generateWord(wordLength);
+
+  try {
+    const newGame = new Game(data);
+    newGame.save();
+    res.send()
+  } catch (e) {
+    res.send(e)
+  }
   // default set to
+  // data.user_id = req.body.user_id || ;
+
 };
 
 module.exports = {
