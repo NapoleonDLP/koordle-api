674c3bbb3be1687e9322f1ada01a5b8a72e37548
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-03 00:00:34 -0800

    Make axios get request to websters api

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
 const randomWord = require('random-word-by-length');
+const axios = require('axios');
 
 const generateWord = (wordLength) => {
   let newWord = randomWord(wordLength);
@@ -10,12 +11,13 @@ const generateWord = (wordLength) => {
   return newWord;
 };
 
-const isWordValid = (word) => {
+const isWordValid = async (word) => {
   const url = process.env.WEBSTER_URL + word + process.env.WEBSTER_KEY;
-  fetch(url)
-    .then(data => data.json())
-    .then(data => console.log("WORD: ", data))
-    .catch(e => console.log('Word Check Error: ', e));
+  console.log(url)
+    const response = await axios.get(url)
+      // .then(data => data.json())
+      .then(data => console.log("WORD: ", data))
+      .catch(e => console.log('Word Check Error: ', e));
 };
 
 module.exports = {
