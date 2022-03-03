88787fa4718a754b900240aa7c4907cef3fc174c
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-02 23:36:07 -0800

    Check word in websters dictionary

--- helpers.js
+++ helpers.js
@@ -10,6 +10,15 @@ const generateWord = (wordLength) => {
   return newWord;
 };
 
+const isWordValid = (word) => {
+  const url = process.env.WEBSTER_URL + word + process.env.WEBSTER_KEY;
+  fetch(url)
+    .then(data => data.json())
+    .then(data => console.log("WORD: ", data))
+    .catch(e => console.log('Word Check Error: ', e));
+};
+
 module.exports = {
   generateWord,
+  isWordValid,
 };
