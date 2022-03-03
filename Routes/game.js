c5bc017454c3437575897cfff3d343e39e963404
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-03 00:00:07 -0800

    Create endpoint to check if word is valid

--- Routes/game.js
+++ Routes/game.js
@@ -1,5 +1,5 @@
 const { DB } = require('../db.js');
-const { generateWord } = require('../helpers.js');
+const { generateWord, isWordValid } = require('../helpers.js');
 const { Game } = require('../Models/Game.js');
 
 //TODO: find a better solution for acquiring random word
@@ -11,9 +11,10 @@ const getNewWord = (req, res) => {
   res.send({ newWord });
 };
 
-//Check if word is a valid word
-const isWordValid = (req, res) => {
+const postWordCheck = (req, res) => {
+  isWordValid(req.body.word);
 
+  res.send(req.body.word);
 };
 
 //Create a new game
@@ -27,9 +28,10 @@ const postNewGame = (req, res) => {
   try {
     const newGame = new Game(data);
     newGame.save();
-    res.send()
+    res.send(newGame);
   } catch (e) {
-    res.send(e)
+    console.error(e);
+    res.status(500);
   }
   // default set to
   // data.user_id = req.body.user_id || ;
@@ -39,5 +41,5 @@ const postNewGame = (req, res) => {
 module.exports = {
   getNewWord,
   postNewGame,
-  isWordValid,
+  postWordCheck,
 };
