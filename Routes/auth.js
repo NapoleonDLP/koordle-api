0c55f4b0107cbef0af4251e298303c4dd3994253
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-17 23:11:28 -0700

    Res cookie with token

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,26 +1,36 @@
 //Add any dependencies
 const bcrypt = require('bcrypt');
 const { User } = require('../Models/User.js');
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET_STRING;
 
 const authenticate = async (req, res) => {
   const { userEmail, password } = req.body;
   const user = await User.findOne({ email: userEmail });
+
+  console.log("GALLETAS: ", req.cookies)
 
   if (!user) {
     res.status(400).json({ error: 'Email not found' });
     return;
   }
 
-  bcrypt.compare(password, user.password, (err, same) => {
+  await bcrypt.compare(password, user.password, async (err, same) => {
     if (err) {
       console.log("ERR: ", err);
       res.status(500);
+    } else if (!same) {
+      res.status(401).json({ error: 'Password incorrect' });
     } else {
-      //send JWT token
-      console.log('SAMSIES: ', same, err);
+      const payload = { userEmail };
+      console.log('LOAD: ', payload)
+      const token = await jwt.sign(payload, secret, {
+        expiresIn: '2 days'
+      });
+      res.cookie('token', token, { httpOnly: true })
+        .sendStatus(200);
     }
-  })
-  console.log("USER FROM AUTH/DB: ", user);
+  });
 };
 
 module.exports = {
