c83d1f1120b2acd7f63c1b1f1a50f94ba34fb327
NapoleonDLP <napoleon.dlp@gmail.com>
2022-03-17 22:44:53 -0700

    Compare password using bcrypt

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,16 +1,26 @@
 //Add any dependencies
-
+const bcrypt = require('bcrypt');
+const { User } = require('../Models/User.js');
 
 const authenticate = async (req, res) => {
   const { userEmail, password } = req.body;
   const user = await User.findOne({ email: userEmail });
 
   if (!user) {
-    res.status(400).json({ error: 'User not found' });
+    res.status(400).json({ error: 'Email not found' });
     return;
   }
 
-
+  bcrypt.compare(password, user.password, (err, same) => {
+    if (err) {
+      console.log("ERR: ", err);
+      res.status(500);
+    } else {
+      //send JWT token
+      console.log('SAMSIES: ', same, err);
+    }
+  })
+  console.log("USER FROM AUTH/DB: ", user);
 };
 
 module.exports = {
